a0e7876accca1866f09e30f6db2852ab7bf90f0f
abhi-eternal <166623508+abhi-eternal@users.noreply.github.com>
2024-05-16 11:09:14 +0530

    Add files via upload

---
+++ Eternal-app/Eternal_app/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Developed by Eternal
+# See LICENSE file for full copyright and licensing details
+{
+    'name': 'Eternal App',
+    'version': '17.0.0.0',
+    'summary': 'Eternal Ap Shop',
+    'description': """ Eternal Theme """,
+    'category': 'App',
+    'author': 'Eternal',
+    'maintainer': 'Eternal',
+    'website':'https://www.eternalsoftsolutions.com',
+    'license':'LGPL-3',
+
+    'depends': [
+        'base',
+        'product',
+        'sale_management',
+        'purchase',
+        'stock'
+    ],
+    'data': [
+    ],
+    'images':'static/description/banner.png',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'price': 49.99,
+    'currency': 'EUR'
+}
